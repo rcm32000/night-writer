c65192a984b029c1874fe2e17aee062938b72685
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-03-26 17:15:46 -0600

    convert the arrays into strings

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -7,17 +7,20 @@ class NightWriter
 					:bottom_row
   def initialize(message = 'hello')
     @message = message
-	@top_row = []
-	@middle_row = []
-	@bottom_row = []
+	@top_row = ""
+	@middle_row = ""
+	@bottom_row = ""
   end
 
   def convert(message)
+	  @top_row = ""
+  	@middle_row = ""
+  	@bottom_row = ""
     message.chars.map do |character|
      @top_row << braille_alphabet[character].split("\n")[0]
 	 @middle_row << braille_alphabet[character].split("\n")[1]
 	 @bottom_row << braille_alphabet[character].split("\n")[2]
     end
-	(@top_row +@middle_row +@bottom_row )
+	[@top_row,@middle_row,@bottom_row].join("\n")
   end
 end
