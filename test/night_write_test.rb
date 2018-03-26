aa73325f19d21c46019c3bdd31291b9972c36dc8
rcm32000 <rcm32000@yahoo.com>
2018-03-26 13:21:48 -0600

    Braille library created

--- test/night_write_test.rb
+++ test/night_write_test.rb
@@ -11,8 +11,35 @@ class TestNightWriter < Minitest::Test
   end
 
   def test_convert_one_letter
+    skip
     nw = NightWriter.new
 
     assert_equal "0-\n--\n--", nw.convert('a')
+  end
+
+  def test_row_creation
+    skip
+    nw = NightWriter.new
+
+    assert_equal "0-\n--\n--", nw.convert('a')
+    assert_equal "0-", nw.top_row
+    assert_equal "--", nw.middle_row
+    assert_equal "--", nw.bottom_row
+
+    assert_equal "00\n-0\n0-", nw.convert('n')
+    assert_equal "00", nw.top_row
+    assert_equal "-0", nw.middle_row
+    assert_equal "0-", nw.bottom_row
+  end
+
+  def test_convert_word
+    skip
+    nw = NightWriter.new
+    expected = "0-\n"
+               "00\n"
+               "--\n"
+    given = nw.convert('hello')
+
+    assert_equal expected, given
   end
 end
