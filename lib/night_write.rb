902855cfb46e77c772409f56ee7d307b50f263e7
rcm32000 <rcm32000@yahoo.com>
2018-03-26 17:22:21 -0600

    Convert sentence success

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -2,13 +2,22 @@ require './lib/braille_lib'
 
 class NightWriter
   include Braille
+  attr_reader :top_row,
+              :middle_row,
+              :bottom_row
   def initialize(message = 'hello')
     @message = message
   end
 
   def convert(message)
+    @top_row = ""
+    @middle_row = ""
+    @bottom_row = ""
     message.chars.map do |character|
-      braille_alphabet[character]
+      @top_row << braille_alphabet[character].split("\n")[0]
+      @middle_row << braille_alphabet[character].split("\n")[1]
+      @bottom_row << braille_alphabet[character].split("\n")[2]
     end
+    [@top_row, @middle_row, @bottom_row].join("\n")
   end
 end
