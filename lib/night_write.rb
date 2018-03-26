b20ba0ca48b3c3d9ee1b13d942d753916c7dd271
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-03-26 17:06:44 -0600

    Adding together the rows to create the 3 line brail

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -2,13 +2,22 @@ require './lib/braille_lib'
 
 class NightWriter
   include Braille
+  attr_reader :top_row,
+  					:middle_row,
+					:bottom_row
   def initialize(message = 'hello')
     @message = message
+	@top_row = []
+	@middle_row = []
+	@bottom_row = []
   end
 
   def convert(message)
     message.chars.map do |character|
-      braille_alphabet[character]
+     @top_row << braille_alphabet[character].split("\n")[0]
+	 @middle_row << braille_alphabet[character].split("\n")[1]
+	 @bottom_row << braille_alphabet[character].split("\n")[2]
     end
+	(@top_row +@middle_row +@bottom_row )
   end
 end
